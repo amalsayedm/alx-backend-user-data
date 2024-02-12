07be7045c5955c8610c6e1a521f4d0b8788e205d
amalsayedm <amal.sayedm2@gmail.com>
2024-02-12 21:54:14 +0200

    logger

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""module for authentication"""
+import re
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """class auth for api authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication."""
+        return False
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Gets the current user from the request"""
+        return None
+
+    def authorization_header(self, request=None) -> str:
+        """Gets the authorization header field from the request."""
+        return None
