e72218bc653823d9fb39ac013723a4526f48b660
amalsayedm <amal.sayedm2@gmail.com>
2024-02-19 23:03:17 +0200

    session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,3 +12,11 @@ class SessionAuth(Auth):
     """Session authentication class.
     """
     user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for the user.
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
